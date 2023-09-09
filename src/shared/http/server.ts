cb4ecac700482baad6d0ed62c51de08f79113f24
Gabriel Jesus <gabriel.developer2@gmail.com>
2023-09-09 15:47:27 -0300

    add cors and first route

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,14 @@
-export class AppServer {
-  private app: string
+import express from 'express'
+import 'express-async-errors'
+import cors from 'cors'
+const app = express()
 
-  constructor(info: string) {
-    this.app = info ?? 'Olá dev'
-  }
-}
+app.use(express.json())
+app.use(cors())
+app.get('/', (request, response) => {
+  return response.json({ message: 'Olá' })
+})
+
+app.listen(3000, () => {
+  console.log('listening on port 3000🚀')
+})
