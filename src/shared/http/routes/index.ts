f84e877ce57801654d9c2e06cc43ecc71cbb4339
Gabriel Jesus <gabriel.developer2@gmail.com>
2023-09-09 16:29:12 -0300

    add roles route

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
+import { rolesRoutes } from '@roles/http/routes/roles.route'
 import { Router } from 'express'
 
 const routes = Router()
 
 routes.get('/', (request, response) => {
-  throw new Error('Acesso negado!', 401)
+  return response.json({ message: 'Hello world!' })
 })
 
+routes.use('/roles', rolesRoutes)
 export { routes }
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from 'express'
+import express, { Request, Response } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
 import 'dotenv/config'
@@ -11,17 +11,15 @@ app.use(cors())
 
 app.use(routes)
 
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      response
-        .status(error.statusCode)
-        .json({ status: 'error', message: error.message })
-    }
-    console.log(error)
-    return response.status(500).json({ message: 'Internal Server Error' })
-  },
-)
+app.use((error: Error, request: Request, response: Response) => {
+  if (error instanceof AppError) {
+    response
+      .status(error.statusCode)
+      .json({ status: 'error', message: error.message })
+  }
+  console.log(error)
+  return response.status(500).json({ message: 'Internal Server Error' })
+})
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}🚀`)
 })
